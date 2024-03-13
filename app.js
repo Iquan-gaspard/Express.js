71192294c2f06e6fa24212529c535920cf8d94ec
Iquan <z98428222@gmail.com>
2024-03-13 22:18:36 +0800

    Static Files

--- app.js
+++ app.js
@@ -1,25 +1,12 @@
 const express = require("express");
 const app = express();
 
-//middlewares
-app.use((req, res, next) => {
-  console.log("passing middlewares");
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log("passing second middlewares");
-  next();
-});
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.send("welcome");
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/example", (req, res) => {
-  res.redirect("/actualexample");
-});
-//
 //port callback
 app.listen(3000, () => {
   console.log("server is listen 3000");
